f99e4ff3e64ffb3a208d6f8f17836daabcd058d2
weapon-xx <88180569@qq.com>
2019-02-20 10:07:21 +0800

    feat: modify eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 /**
- * eslint 配置文件
+ * eslint config
  *
  */
 
@@ -9,7 +9,7 @@ module.exports = {
     node: true
   },
   globals: {
-    isBrowser: true
+    // 
   },
   extends: [
     'plugin:vue/essential',
@@ -17,32 +17,40 @@ module.exports = {
     'airbnb',
   ],
   rules: {
-    // 部分变量允许使用下划线开头和结尾定义变量（主要是因为__INITIAL_STATE__没法改）
+    // 4 space indent
+    // 'indent': ['error', 4],
+
+    // max character in single line
+    'max-len': [1, 150, 4],
+
+    // allow console
+    'no-console': 0,
+
+    // allow debugger in development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+    // allow alert in development
+    'no-alert':  process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+    // max nest callback 3
+    'max-nested-callbacks': ['error', 3],
+
+    // allow underscore variable
+    // such as: __INITIAL_STATE__, __VUE_SSR_CONTEXT__
     'no-underscore-dangle': ['error', {
       allow: [
-        '__INITIAL_STATE__', // Vue同步状态
-        '__VUE_SSR_CONTEXT__', // Vue服务端渲染context
+        '__INITIAL_STATE__',
+        '__VUE_SSR_CONTEXT__',
       ],
     }],
 
-    /**
-     * 覆盖airbnb不允许对参数赋值的规则，可以对参数的属性进行修改
-     *
-     * @see https://github.com/airbnb/javascript/issues/1217
-     * @see https://github.com/airbnb/javascript#functions--mutate-params
-     */
+    // allow reassign param
     'no-param-reassign': [
       'error',
       {
-        props: false,
-      },
+          'props': false,
+      }
     ],
-
-    // 函数嵌套层数，最大3层
-    'max-nested-callbacks': ['error', 3],
-
-    // 行最大字符数 150
-    'max-len': [1, 150, 4]
   },
   parserOptions: {
     parser: 'babel-eslint',
